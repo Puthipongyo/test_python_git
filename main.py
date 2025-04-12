a47a00388b1d5d8eb0f75efe2b118fecad2d5d02
puthipong <puthipong.yom@gmail.com>
2025-04-12 09:47:56 +0700

    while loop

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+import pandas as pd
+
+if __name__ == "__main__":
+    print("Loading data...")
+    running = True
+    count = 0
+    while running:
+        print(count)
+        count +=1
+        time.sleep(1)
+        if count==3 : running = False
